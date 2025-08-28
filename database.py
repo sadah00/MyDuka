0e5e7a564d7dab323df4784ead7ca4b15567173c
sadah00 <kimberlysada30@gmail.com>
2025-08-28 16:16:32 +0300

    database.py

--- database.py
+++ database.py
@@ -4,14 +4,52 @@ import psycopg2
 conn = psycopg2.connect(host="localhost", port="5432", user="postgres", password="kimysada6",dbname="myduka_db")
 cur=conn.cursor()
 
-cur.execute("SELECT * FROM products")
+# displaying products
+def get_products():
+    cur.execute("SELECT * FROM products")
+    products = cur.fetchall()
+    return products
 
-products = cur.fetchall()
-
+products = get_products()
 print(products)
 
-cur.execute("SELECT * FROM sales")
+# displaying sales
+def get_sales():
+    cur.execute("SELECT * FROM sales")
+    sales = cur.fetchall()
+    return sales
 
-sales = cur.fetchall()
-
+sales=get_sales()
 print(sales)
+
+# insert products
+def insert_products(product_values):
+    cur.execute(f"INSERT INTO products (name,buying_price,selling_price) VALUES{product_values}")
+    conn.commit()
+# input from user
+name= input("Enter product name: ")
+buying_price= float(input("Enter buying price: "))
+selling_price=float(input("Enter selling price: "))
+
+product_values=(name,buying_price,selling_price)
+
+insert_products(product_values)
+
+# inserting more products
+
+# products1=('flour',140,180)
+# products2=('sugar',120,150)
+# insert_products(products1)
+# insert_products(products2)
+
+# insert sales
+def insert_sales(sale_values):
+    cur.execute(f"INSERT INTO sales (pid, quantity) VALUES {sale_values}")
+    conn.commit()
+
+pid=int(input("Enter product id: "))
+quantity=int(input("Enter quantity: "))
+
+sale_values=(pid,quantity)
+
+insert_sales(sale_values)

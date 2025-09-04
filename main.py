9d31e2b2db92014a1a5923b3da44ccc9f983621a
sadah00 <kimberlysada30@gmail.com>
2025-09-05 01:07:24 +0300

    main.py

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-from flask import Flask , render_template
+from flask import Flask , render_template 
 
 # creating a flask instance
 app = Flask(__name__)
 
 @app.route('/')  # unique routes
 def home():  # must have a unique name
-    return "My index route"
- 
-@app.route('/contact') # decorator function  
-def contact(): # View function - giving back data
-    return "Contact Us"
+    return render_template("index.html")
+
+@app.route('/dashboard') # decorator function
+def dashboard(): # View function - giving back data
+    return render_template("dashboard.html")
 
 @app.route('/login')
 def login():
-    return "Sign in"
+    return render_template("login.html")
 
-@app.route('/Register')
+@app.route('/register')
 def register():
-    return "Sign up"
+    return render_template("register.html")
 
-@app.route('/info')
-def info():
-    return "Display more information"
+@app.route('/stock')
+def stock():
+    return render_template("stock.html")
 
 @app.route('/products')
 def products():
     products = ["Bread","Milk","Eggs"]
-    return products
+    return render_template("products.html")
 
 @app.route('/sales')
 def sales():
     sales = {"id 1":100, "id 2":200, "id 3":300}
-    return sales
+    return render_template("sales.html")
 
 
 
